1500593ca6942f4402303e426967eca2c0c15ad8
sajib689 <jibonraja11@gmail.com>
2022-10-13 18:56:23 +0600

    second

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,18 +1,23 @@
 
 import React, { useState } from 'react';
-
-import { useLoaderData } from 'react-router-dom';
-import { removeFromDb } from '../../utilities/fakedb';
+import { useLoaderData, Link } from 'react-router-dom';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-
 const Orders = () => {
     const {products, intialCart} = useLoaderData();
     const [cart, setCart] = useState(intialCart)
+    
+    
     const handleRemoveItem = (id) => {
         const remaining = cart.filter(product => product.id !== id);
         setCart(remaining);
         removeFromDb(id)
+    }
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
     }
     return (
         <div className='shop-container'>
@@ -24,9 +29,12 @@ const Orders = () => {
                    handleRemoveItem = {handleRemoveItem}
                     ></ReviewItem>)
                 }
+                {
+                    cart.length === 0 && <h2>No Product Found <Link to='/shop'>Shop</Link></h2>
+                }
             </div>
             <div className='cart-container'>
-            <Cart cart={cart}></Cart>
+            <Cart clearCart={clearCart} cart={cart}></Cart>
             </div>
         </div>
     );
